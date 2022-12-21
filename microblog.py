37e2d1cf362e3f13fd162496e9767082bf7bac51
Dusan Rangelov <dusanrangelov@yahoo.com>
2022-12-21 22:32:43 +0100

    Adding ssl functionality

--- microblog.py
+++ microblog.py
@@ -3,9 +3,13 @@ from app.models import Message, Notification, User, Post
 
 
 app = create_app()
-app.run()
+
 
 
 @app.shell_context_processor
 def make_shell_context():
     return {'db':db, 'User':User, 'Post':Post, 'Notification': Notification, 'Message': Message}
+
+
+if __name__ == "__main__":
+    app.run(ssl_context=('cert.pem','key.pem'))
